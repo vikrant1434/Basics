9a266fed01433ecb83516273bf1e998965d9003c
Vikrant Rana <89657262+vikrant1434@users.noreply.github.com>
2024-01-26 13:58:07 +0530

    BasicsOfJavaScript

---
+++ basicPrograms.js
@@ -0,0 +1,74 @@
+// how to print in javascript
+console.log("hello vikrant")
+
+// program to find area of a circle
+
+let radious = 5
+let pie = 3.14
+let area = pie*radious*radious
+console.log("area of the circle is:" + area);
+
+// program to find out area of a rectangle
+// formula: area of a rectange = height*width
+
+let height = 5
+let width = 10
+let rectangleArea = height*width
+console.log("area of the rectange is:" +rectangleArea);
+
+// area of a cuboid formula- 2*(l*b+b*h+h*l)
+
+let length = 10
+let breadth = 7
+let heightis = 5
+let areaIs = 2*(length*breadth+breadth*height+height*length)
+console.log("area of a cuboid is:" +areaIs);
+
+// right angle triangle area=1/2*base*height
+
+let heightOfTriangle = 10
+let baseOfTriangle = 5
+let areaOfTriangle = 1/2*baseOfTriangle*heightOfTriangle
+console.log("area of a right amgle triangle is :"+areaOfTriangle);
+
+// area of a square = side*side 
+
+let side = 20
+let areaOfSquare = side*side
+console.log("area of a square is :"+areaOfSquare);
+
+// swap a variable's value 
+
+let first = 5;
+let second = 10;
+console.log(`before swaping the value of first and second are ${first}, ${second}`)
+
+// Using a temporary variable
+let temp = first;
+first = second;
+second = temp;
+
+console.log(`after swaping the value of first and second are ${first}, ${second}`);
+
+// convert temperature from celcious to fahrenheit
+// temperature in celcious
+let tempInCel = 50
+
+// convert celcious into fehrenheit 
+let tempInFehren = (tempInCel*9/5)+32
+console.log(`celcious temperature in fehreinheit is :${tempInFehren}`);
+
+
+// convert fehrenheit temperature into celcious
+
+let temperatureInFehren = 75
+let temperatureInCelcious = (temperatureInFehren-32)*5/9
+console.log(`celcious to fehrenheit temperature:${temperatureInCelcious}`);
+
+
+
+
+
+
+
+
